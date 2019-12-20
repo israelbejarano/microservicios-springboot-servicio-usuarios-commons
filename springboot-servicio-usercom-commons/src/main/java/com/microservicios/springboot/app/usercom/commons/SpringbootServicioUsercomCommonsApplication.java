755f5d08be761b344d60bc6da756502ce0a35edd
israel <israel.bejarano.a@gmail.com>
2019-12-20 17:22:42 +0100

    solved error de nombre paquetes + conflicto

--- springboot-servicio-usercom-commons/src/main/java/com/microservicios/springboot/app/usercom/commons/SpringbootServicioUsercomCommonsApplication.java
+++ springboot-servicio-usercom-commons/src/main/java/com/microservicios/springboot/app/usercom/commons/SpringbootServicioUsercomCommonsApplication.java
@@ -1,13 +1,11 @@
 package com.microservicios.springboot.app.usercom.commons;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class SpringbootServicioUsercomCommonsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootServicioUsercomCommonsApplication.class, args);
-	}
 
 }
